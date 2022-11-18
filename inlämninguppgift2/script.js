43716b1a481223e82011607ed5680c6c1d6d6ab6
SehamJad <113519633+SehamJad@users.noreply.github.com>
2022-11-18 18:27:55 +0100

    Update script.js
    create the second fetch fun and bring weather, time

--- inlämninguppgift2/script.js
+++ inlämninguppgift2/script.js
@@ -1,34 +1,55 @@
-//let APIkey = '5075d36c231e7facbda06812845f3d10';
-let latitude;
+let APIkey = '5075d36c231e7facbda06812845f3d10';// declare a  valid APIkey as global variable
+// create aglobal variables
+let latt;
 let long;
-var temp = document.querySelector("#Weather Temp")
+let temprature;
+let country;
+let city;
 
-// grape geo location for Stockholm city
 async function binrgLocalinfo() {
-  
- const result = await  fetch(`http://api.openweathermap.org/geo/1.0/direct?q=Helsingborg&limit=1&appid=${APIkey}`) // to get latitude longitude for the local city
-  const data = await result.json();
-  console.log(data);
-    console.log(data[0].name);
-    console.log(data[0].lat);
-    console.log(data[0].lon);
-    let country = data[0].country;
-    let latitude = data[0].lat;
+  // grape geo location for Stockholm city, AJAX med fetch ,
+
+  const result = await  fetch(`http://api.openweathermap.org/geo/1.0/direct?q=Stockholm&limit=1&appid=${APIkey}`) // to get latitude longitude for the local city
+  const data = await result.json();// convert the data to json
+  console.log(data);// print the data in the console
+  console.log(data[0].name);
+  console.log(data[0].lat);
+  console.log(data[0].lon);
+    let country = data[0].country;// save the json data to avariable 
+    let latt = data[0].lat;
     let long = data[0].lon;
+    let city = data[0].name;
 
- }
- binrgLocalinfo();
+  const resultt = await  fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${latt}&lon=${long}&limit=1&appid=${APIkey}`)// set the result of the fetch fun to avariable 
+  const datajson = await resultt.json();// convert the data to json
+  console.log(datajson);
 
- async function localWeatherinfo() {
-    binrgLocalinfo();
-      
- const resultt = await  fetch(`http://api.openweathermap.org/geo/1.0/reverse?lat=${latitude}&lon=${long}&limit=1&appid=${APIkey}`)
- const datajson = await resultt.json();
- console.log(datajson);
+ // take the data and display it on the webpage
+   function bringLocalweather(){
 
+       let temprature = datajson.main.temp;
+	   let condition = datajson.weather[0].description;
+	   let icon = datajson.weather[0].icon;
+	   console.log(condition);
+	   var celcius = Math.round(parseFloat(temprature)-273.15);
+       console.log(temprature);
+	   let time = new Date(datajson.dt * 1000);
+	   let hrs = time.getHours();
+	   let mins = time.getMinutes();
+	   let str =`${hrs}:${mins}`;
+       //Dom//
+       document.getElementById('town').innerHTML = city;// display stockholm on the webpage
+       document.getElementById('count').innerHTML = country;// display se on webpage
+	   document.getElementById('tempr').innerHTML = celcius + '&deg;';// display temprture
+	   document.getElementById('cond').innerHTML = condition;// display condition of the weather
+	   document.getElementById('icn').innerHTML = icon;
+	   document.getElementById('time').innerHTML =str;
 
- }
- localWeatherinfo();
+   }
+    bringLocalweather(); // call the second func"bringLocalweather" after "binrgLocalinfo"
+}
+binrgLocalinfo(); // call the function binrgLocalinfo when the page loads 
 
  
-     
+
+ 
